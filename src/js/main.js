0c68660c70f800230e488b394dd7682085dd0ff5
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2019-03-02 20:59:02 +0500

    Added lock of portrait orientation on mobile devices for corrent displaying

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,8 @@ window.addEventListener("load", () => {
 		menu.classList.toggle("menu_visible");
 	});		
 
+	screen.orientation.lock("portrait-primary");
+
 	setThemeBySeason([ document.querySelector(".calendar-header"),
 										 document.querySelector(".day-modal__date-side") ]);
 
